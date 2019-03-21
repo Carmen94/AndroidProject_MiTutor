d77d7b3bd488300917a924190e03c62289c3b84c
Carmen <carmen@RIVERA13>
2019-03-21 08:38:57 -0600

    Activity Main rename

--- app/src/main/java/com/iteso/mitutor/ActivityMain.java
+++ app/src/main/java/com/iteso/mitutor/ActivityMain.java
@@ -3,7 +3,7 @@ package com.iteso.mitutor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
