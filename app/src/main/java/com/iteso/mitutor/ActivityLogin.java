fa8847e8b1c5154de2ea13d26fc0ad271842fd2d
AndreaRayon <is705113@iteso.mx>
2019-05-02 01:40:45 -0500

    idjriv

--- app/src/main/java/com/iteso/mitutor/ActivityLogin.java
+++ app/src/main/java/com/iteso/mitutor/ActivityLogin.java
@@ -1,25 +1,53 @@
 package com.iteso.mitutor;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ActivityLogin extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
+
+import static com.firebase.ui.auth.ui.AcquireEmailHelper.RC_SIGN_IN;
+import static com.google.android.gms.auth.api.signin.GoogleSignIn.*;
+
+public class ActivityLogin extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
     EditText mail, password;
     Button login;
     TextView signup;
 
+    SignInButton signInButton;
+    Button signOutButton;
+    GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mail = findViewById(R.id.activity_username);
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        /*mail = findViewById(R.id.activity_username);
         password = findViewById(R.id.activity_password);
         login = findViewById(R.id.activity_save);
         signup = findViewById(R.id.login_signup);
@@ -42,8 +70,42 @@ public class ActivityLogin extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        });
+        });*/
+    }
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                handleSignInResult(result);
+
+            }
+        }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if(result.isSuccess()) {
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+        } else {}
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.sign_in_button: signIn(); break;
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
