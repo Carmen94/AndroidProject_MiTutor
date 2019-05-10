c2c6653d8b67876ca14820008a95651ca8fae580
Carmen <carmen@RIVERA13>
2019-05-10 10:50:17 -0500

    Removed notifications from chat view

--- app/src/main/java/com/iteso/mitutor/ActivityChat.java
+++ app/src/main/java/com/iteso/mitutor/ActivityChat.java
@@ -1,15 +1,9 @@
 package com.iteso.mitutor;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
@@ -82,7 +76,6 @@ public class ActivityChat extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
               chatAdapter.notifyDataSetChanged();
-              showNotification(context);
             }
 
             @Override
@@ -187,34 +180,6 @@ public class ActivityChat extends AppCompatActivity {
         Intent intent = new Intent(ActivityChat.this,ActivitySearch.class);
         startActivity(intent);
         finish();
-    }
-
-    public void showNotification(Context context){
-        createNotificationChannel(context);
-        Intent intent = new Intent(context, ActivityMain.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default")
-                .setSmallIcon(R.drawable.math_tutor)
-                .setContentTitle(context.getString(R.string.notification_title))
-                .setContentText("Notification message")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-        notificationManager.notify(1, builder.build());
-    }
-
-    private void createNotificationChannel(Context context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = context.getString(R.string.app_name);
-            String description = context.getString(R.string.project_id);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("default", name, importance);
-            channel.setDescription(description);
-            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
     }
 
 }
