c3e9ac97e77762f2f855a78c6fe6569dcccce72a
AndreaRayon <is705113@iteso.mx>
2019-05-12 00:38:16 -0500

    my tutorships added

--- app/src/main/java/com/iteso/mitutor/ActivityProfile.java
+++ app/src/main/java/com/iteso/mitutor/ActivityProfile.java
@@ -1,8 +1,11 @@
 package com.iteso.mitutor;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -11,14 +14,29 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.iteso.mitutor.beans.Subject;
+import com.iteso.mitutor.tools.AdapterAllSubjects;
+
+import java.util.ArrayList;
 
 public class ActivityProfile extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseUser user;
     TextView username;
     TextView mail;
-    String photoUrl;
     ImageView imageView;
+    private ArrayList<Subject> subjects;
+    private RecyclerView.Adapter subjectAdapter;
+    RecyclerView recyclerView;
+    DatabaseReference databaseReference;
+    boolean init=false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +53,34 @@ public class ActivityProfile extends AppCompatActivity {
             Glide.with(this).load(user.getPhotoUrl().toString()).into(imageView);
         }
 
+        recyclerView = findViewById(R.id.all_subjects_recycler_view);
+        subjects = new ArrayList<>();
+        databaseReference =  FirebaseDatabase.getInstance().getReference();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!init){
+                    for(DataSnapshot snapshot : dataSnapshot.child("subjects").getChildren()){
+                        Subject subject = snapshot.getValue(Subject.class);
+                        subjects.add(subject);
+                    }
+                    subjectAdapter.notifyDataSetChanged();
+                    init=true;
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }});
+        subjectAdapter = new AdapterAllSubjects(this,subjects);
+        recyclerView.setAdapter(subjectAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 
     @Override
